745e467dec875e75c63d72ef8d681d49765d1ffb
isuperflying <1365961935@qq.com>
2018-04-23 18:05:54 +0800

    20180423

---
+++ ToosProject/src/com/zdh/test/ToosTest.java
@@ -0,0 +1,14 @@
+package com.zdh.test;
+
+import cn.hutool.core.convert.Convert;
+
+public class ToosTest {
+
+	public static void main(String[] args) {
+		String[] b = { "1", "2", "3", "4" };
+
+		Integer[] intArray = Convert.toIntArray(b);
+		
+	}
+
+}
